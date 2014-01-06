3d1ee2b556ffe093a4053946157a3556ace97c1a
evild70 <dlahay@gmail.com>
2014-01-06 14:34:46 -0600

    Separated Welcome and AskFor. Now asks for Respond.js.

--- app/index.js
+++ app/index.js
@@ -34,139 +34,120 @@ var WebappDelGenerator = module.exports = function WebappDelGenerator(args, opti
 
 util.inherits(WebappDelGenerator, yeoman.generators.Base);
 
-WebappDelGenerator.prototype.askFor = function askFor() {
-  var cb = this.async();
+WebappDelGenerator.prototype.welcome = function askFor() {
 
   // welcome message
   if (!this.options['skip-welcome-message']) {
     console.log(this.yeoman);
+    console.log(chalk.yellow('This is webapp-del. Thanks for giving it a try.'))
     console.log(chalk.magenta('To start with: HTML5 Boilerplate, jQuery, and a Gruntfile.js to build your app.'));
   }
 
-  // var prompts = [{
-  //   type: 'checkbox',
-  //   name: 'features',
-  //   message: 'What more would you like?',
-  //   choices: [{
-  //     name: 'Bootstrap for Sass',
-  //     value: 'compassBootstrap',
-  //     checked: true
-  //   }, {
-  //     name: 'Modernizr',
-  //     value: 'includeModernizr',
-  //     checked: true
-  //   }]
-  // }];
+};
 
-  // this.prompt(prompts, function (answers) {
-  //   var features = answers.features;
-
-  //   function hasFeature(feat) { return features.indexOf(feat) !== -1; }
-
-  //   // manually deal with the response, get back and store the results.
-  //   // we change a bit this way of doing to automatically do this in the self.prompt() method.
-  //   this.compassBootstrap = hasFeature('compassBootstrap');
-  //   this.includeModernizr = hasFeature('includeModernizr');
-
-  //   cb();
-  // }.bind(this));
+WebappDelGenerator.prototype.askForRespond = function askFor() {
+  var cb = this.async();
 
   var prompts = [{
     type: 'confirm',
-    name: 'someOption',
-    message: 'Continue, yo!?',
-    default: true
+    name: 'includeRespond',
+    message: 'Include Respond.js for <IE8 ?',
+    default: false
   }];
 
   this.prompt(prompts, function (props) {
-    this.someOption = props.someOption;
-
+    this.includeRespond = props.includeRespond;
     cb();
   }.bind(this));
 };
 
 WebappDelGenerator.prototype.gruntfile = function gruntfile() {
-  this.template('Gruntfile.js');
+    this.template('Gruntfile.js');
 };
 
 WebappDelGenerator.prototype.packageJSON = function packageJSON() {
-  this.template('_package.json', 'package.json');
+    this.template('_package.json', 'package.json');
 };
 
 WebappDelGenerator.prototype.git = function git() {
-  this.copy('gitignore', '.gitignore');
-  this.copy('gitattributes', '.gitattributes');
+    this.copy('gitignore', '.gitignore');
+    this.copy('gitattributes', '.gitattributes');
 };
 
 WebappDelGenerator.prototype.bower = function bower() {
-  this.copy('bowerrc', '.bowerrc');
-  this.copy('_bower.json', 'bower.json');
+    this.copy('bowerrc', '.bowerrc');
+    this.copy('_bower.json', 'bower.json');
 };
 
 WebappDelGenerator.prototype.jshint = function jshint() {
-  this.copy('jshintrc', '.jshintrc');
+    this.copy('jshintrc', '.jshintrc');
 };
 
 WebappDelGenerator.prototype.editorConfig = function editorConfig() {
-  this.copy('editorconfig', '.editorconfig');
+    this.copy('editorconfig', '.editorconfig');
 };
 
 WebappDelGenerator.prototype.h5bp = function h5bp() {
-  this.copy('favicon.ico', 'app/favicon.ico');
-  this.copy('404.html', 'app/404.html');
-  this.copy('robots.txt', 'app/robots.txt');
-  this.copy('htaccess', 'app/.htaccess');
+    this.copy('favicon.ico', 'app/favicon.ico');
+    this.copy('404.html', 'app/404.html');
+    this.copy('robots.txt', 'app/robots.txt');
+    this.copy('htaccess', 'app/.htaccess');
 };
 
 WebappDelGenerator.prototype.mainStylesheet = function mainStylesheet() {
-  // var css = 'main.' + (this.compassBootstrap ? 's' : '') + 'css';
-  // this.copy('master.scss', 'app/styles/sass/master.scss');
-  this.bulkDirectory('sass', 'app/styles/sass');
+    // var css = 'main.' + (this.compassBootstrap ? 's' : '') + 'css';
+    // this.copy('master.scss', 'app/styles/sass/master.scss');
+    this.bulkDirectory('sass', 'app/styles/sass');
 };
 
 WebappDelGenerator.prototype.writeIndex = function writeIndex() {
-  var rspnd;
+    var sourceFileList = ['bower_components/jquery/jquery.js'],
+        dflt = ['scripts/plugins.js', 'scripts/main.js'],
+        rspnd = [];
 
-  this.indexFile = this.readFileAsString(path.join(this.sourceRoot(), 'index.html'));
-  this.indexFile = this.engine(this.indexFile, this);
+    this.indexFile = this.readFileAsString(path.join(this.sourceRoot(), 'index.html'));
+    this.indexFile = this.engine(this.indexFile, this);
 
-  rspnd = 'bower_components/respond/dest/';
+    // Include Respond.js?
+    if (this.includeRespond) {
+        rspnd = ['bower_components/respond/dest/respond.matchmedia.addListener.min.js',
+        'bower_components/respond/dest/respond.min.js'];
+    }
 
-  this.indexFile = this.appendScripts(this.indexFile, 'scripts/plugins.js', [
-    'scripts/plugins.js',
-    rspnd + 'respond.matchmedia.addListener.min.js',
-    rspnd + 'respond.min.js']);
+    // concat scripts
+    sourceFileList = sourceFileList.concat(rspnd, dflt);
 
-  this.indexFile = this.appendFiles({
-    html: this.indexFile,
-    fileType: 'js',
-    optimizedPath: 'scripts/main.js',
-    sourceFileList: ['scripts/main.js'],
-    searchPath: '{app,.tmp}'
-  });
+    this.indexFile = this.appendFiles({
+        html: this.indexFile,
+        fileType: 'js',
+        optimizedPath: 'scripts/main.js',
+        sourceFileList: sourceFileList,
+        searchPath: '{app,.tmp}'
+    });
+
 };
 
 WebappDelGenerator.prototype.app = function app() {
-  this.mkdir('app');
-  this.mkdir('app/scripts/vendor');
-  this.mkdir('app/styles/sass');
-  this.mkdir('app/images');
-  this.write('app/index.html', this.indexFile);
+    this.mkdir('app');
+    this.mkdir('app/scripts/vendor');
+    this.mkdir('app/styles/sass');
+    this.mkdir('app/images');
+    this.write('app/index.html', this.indexFile);
 
-  this.write('app/scripts/main.js', 'console.log(\'\\\'Allo \\\'Allo!\');');
+    this.write('app/scripts/main.js', 'console.log(\'\\\'Allo \\\'Allo!\');');
 
-  this.copy('plugins.js', 'app/scripts/plugins.js');
+    this.copy('plugins.js', 'app/scripts/plugins.js');
 };
 
-WebappDelGenerator.prototype.install = function () {
-  if (this.options['skip-install']) {
-    return;
-  }
+// WebappDelGenerator.prototype.install = function () {
+//   if (this.options['skip-install']) {
+//     return;
+//   }
 
-  var done = this.async();
-  this.installDependencies({
-    skipMessage: this.options['skip-install-message'],
-    skipInstall: this.options['skip-install'],
-    callback: done
-  });
-};
+//   var done = this.async();
+//   this.installDependencies({
+//     skipMessage: this.options['skip-install-message'],
+//     skipInstall: this.options['skip-install'],
+//     callback: done
+//   });
+// };
